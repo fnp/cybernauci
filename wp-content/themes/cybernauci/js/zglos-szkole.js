1b4f7a7f49114b97f5c73781b5798e0b080ce3c6
CainIsh <mariusz.machuta@gmail.com>
2016-04-01 13:55:50 +0200

    lista szkol

---
+++ wp-content/themes/cybernauci/js/zglos-szkole.js
@@ -0,0 +1,131 @@
+window.onload = function () {
+    var main = jQuery('#rejestracja .ninja-forms-cont'),
+        wojewodztwoList = jQuery(jQuery.map(main.find('select option'), function (val) {
+            if (val.text == "Wybierz województwo *") {
+                return val;
+            }
+        })[0]).parent(),
+        miejscowoscList = jQuery(jQuery.map(main.find('select option'), function (val) {
+            if (val.text == "Wybierz miejscowość *") {
+                return val;
+            }
+        })[0]).parent(),
+        typSzkolyList = jQuery(jQuery.map(main.find('select option'), function (val) {
+            if (val.text == "Wybierz typ szkoły *") {
+                return val;
+            }
+        })[0]).parent(),
+        nazwaSzkolyList = jQuery(jQuery.map(main.find('select option'), function (val) {
+            if (val.text == "Nazwa szkoły *") {
+                return val;
+            }
+        })[0]).parent(),
+        szkolaUlica = main.find('input[value="Ulica *"]:visible'),
+        szkolaNumer = main.find('input[value="Numer"]:visible'),
+        szkolaKod = main.find('input[value="Kod pocztowy *"]:visible'),
+        wojewodztwo = function () {
+            jQuery.ajax({
+                url: "/wp-content/themes/cybernauci/json/wojewodztwa.json",
+                success: function (res) {
+                    for (var i = 0; i < res.length; i++) {
+                        wojewodztwoList.append(
+                            jQuery('<option></option>').attr('value', res[i].id).text(res[i].nazwa)
+                        )
+                    }
+                    wojewodztwoList.on('change', function (e) {
+                        clearMiejscowosc();
+                        clearSzkoly();
+                        miejscowosc(this.value);
+                    })
+                }
+            });
+        },
+        miejscowosc = function (wojewodztwo_id) {
+            jQuery.ajax({
+                url: "/wp-content/themes/cybernauci/json/miejscowosci/" + wojewodztwo_id + ".json",
+                success: function (res) {
+                    for (var i = 0; i < res.length; i++) {
+                        miejscowoscList.append(
+                            jQuery('<option></option>').attr('value', res[i].id).text(res[i].nazwa)
+                        )
+                    }
+                    miejscowoscList.on('change', function (e) {
+                        clearSzkoly();
+                        szkoly(this.value);
+                    })
+                }
+            });
+        },
+        szkoly = function (miejscowosc_id) {
+            var typSzkoly = [];
+            jQuery.ajax({
+                url: "/wp-content/themes/cybernauci/json/szkoly/" + miejscowosc_id + ".json",
+                success: function (res) {
+                    for (var i = 0; i < res.length; i++) {
+                        nazwaSzkolyList.append(
+                            jQuery('<option></option>').attr({
+                                'value': res[i].id,
+                                'data-typ': res[i].typ,
+                                'data-ulica': res[i].ulica,
+                                'data-numer': res[i].numer,
+                                'data-kod': res[i].kod
+                            }).text(res[i].nazwa)
+                        );
+                        if (jQuery.inArray(res[i].typ, typSzkoly) == -1) typSzkoly.push(res[i].typ);
+                    }
+                    jQuery.unique(typSzkoly);
+                    for (var i = 0; i < typSzkoly.length; i++) {
+                        typSzkolyList.append(
+                            jQuery('<option></option>').attr('value', typSzkoly[i]).text(typSzkoly[i])
+                        );
+                    }
+                    nazwaSzkolyList.on('change', function (e) {
+                        var szkola = jQuery("option:selected", this);
+
+                        typSzkolyList.val(szkola.attr('data-typ'));
+                        szkolaUlica.val(szkola.attr('data-ulica'));
+                        szkolaNumer.val(szkola.attr('data-numer'));
+                        szkolaKod.val(szkola.attr('data-kod'));
+                    });
+                    typSzkolyList.on('change', function (e) {
+                        var typ = this.value;
+
+                        if (typ == "") {
+                            nazwaSzkolyList.find('option').show();
+                        } else {
+                            nazwaSzkolyList.find('option').filter(function (itm) {
+                                if (itm.attr('data-typ') == typ) {
+                                    itm.show();
+                                } else {
+                                    itm.hide();
+                                }
+                            });
+                        }
+                    })
+                }
+            });
+        },
+        clearMiejscowosc = function () {
+            jQuery.each(miejscowoscList.find('options'), function () {
+                if (jQuery(this).attr('value').length !== 0) {
+                    jQuery(this).remove();
+                }
+            })
+        },
+        clearSzkoly = function () {
+            jQuery.each(typSzkolyList.find('options'), function () {
+                if (jQuery(this).attr('value').length !== 0) {
+                    jQuery(this).remove();
+                }
+            });
+            jQuery.each(nazwaSzkolyList.find('options'), function () {
+                if (jQuery(this).attr('value').length !== 0) {
+                    jQuery(this).remove();
+                }
+            });
+            szkolaUlica.val('Ulica *');
+            szkolaNumer.val('Numer');
+            szkolaKod.val('Kod pocztowy *');
+        };
+    wojewodztwo();
+};
--- wp-content/themes/cybernauci/page-zglos-szkole.php
+++ wp-content/themes/cybernauci/page-zglos-szkole.php
@@ -1,30 +1,32 @@
 <?php get_header(); ?>
 
-<div id="rejestracja">
-    <div class="info container-block">
-        <div class="container mainblock">
-            <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-left-computer.png"
-                 class="sidebanner sidebanner-left" alt=""/>
-            <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-right-rockets.png"
-                 class="sidebanner sidebanner-right" alt=""/>
+    <div id="rejestracja">
+        <div class="info container-block">
+            <div class="container mainblock">
+                <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-left-computer.png"
+                     class="sidebanner sidebanner-left" alt=""/>
+                <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-right-rockets.png"
+                     class="sidebanner sidebanner-right" alt=""/>
 
-            <header class="entry-header">
-                <h2 class="entry-title">Formularz rejestracyjny</h2>
-            </header>
-            <div class="entry-content">
-                <div class="col-xs-12 col-md-10 col-md-offset-1">
-                    <?php echo get_field('rejestracja_wprowadzenie'); ?>
-                </div>
-                <div class="innerBlock col-xs-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-                    <? the_content(sprintf(
-                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
-                        get_the_title()
-                    ));
-                    ?>
+                <header class="entry-header">
+                    <h2 class="entry-title">Formularz rejestracyjny</h2>
+                </header>
+                <div class="entry-content">
+                    <div class="col-xs-12 col-md-10 col-md-offset-1">
+                        <?php echo get_field('rejestracja_wprowadzenie'); ?>
+                    </div>
+                    <div class="innerBlock col-xs-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                        <? the_content(sprintf(
+                            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
+                            get_the_title()
+                        ));
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <script src="<?php bloginfo('template_directory'); ?>/js/zglos-szkole.js"></script>
 
 <?php get_footer(); ?>
