c90dbf2507b08a956d2ce1327f8478edd6e9c782
CainIsh <mariusz.machuta@gmail.com>
2016-04-05 11:07:03 +0200

    Inna szkoła

--- wp-content/themes/cybernauci/js/zglos-szkole.js
+++ wp-content/themes/cybernauci/js/zglos-szkole.js
@@ -80,7 +80,7 @@ window.onload = function () {
                     nazwaSzkolyList.on('change', function (e) {
                         var szkola = jQuery("option:selected", this);
 
-                        if (this.value == '(Szkoła z poza listy)') {
+                        if (this.value == '' && szkola.text() == '(Szkoła z poza listy)') {
                             szkolaAddNew.modal('show')
                         } else {
                             typSzkolyList.val(szkola.attr('data-typ'));
