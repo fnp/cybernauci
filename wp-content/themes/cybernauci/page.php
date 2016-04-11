4e958af77d95738f4e49d41fe0b03bd8355b3f54
Cain-Ish <mariusz.machuta@gmail.com>
2016-04-11 09:30:52 +0200

    plugin updates

--- wp-content/themes/cybernauci/page.php
+++ wp-content/themes/cybernauci/page.php
@@ -8,63 +8,63 @@
             <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-right-planet.png"
                  class="sidebanner sidebanner-right" alt=""/>
         </div>
-    </div>
 
-    <div class="aktualnosci-list container-block">
-        <div class="container mainblock">
-            <?php
-            while (have_posts()) : the_post();
-                ?>
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <?php if (is_sticky() && is_home() && !is_paged()) : ?>
-                            <span class="sticky-post"><?php _e('Featured', 'cybernauci'); ?></span>
-                        <?php endif; ?>
+        <div class="aktualnosci-list">
+            <div class="container mainblock">
+                <?php
+                while (have_posts()) : the_post();
+                    ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <header class="entry-header">
+                            <?php if (is_sticky() && is_home() && !is_paged()) : ?>
+                                <span class="sticky-post"><?php _e('Featured', 'cybernauci'); ?></span>
+                            <?php endif; ?>
 
-                        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-                    </header><!-- .entry-header -->
+                            <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+                        </header><!-- .entry-header -->
 
-                    <?php cybernauci_excerpt(); ?>
+                        <?php cybernauci_excerpt(); ?>
 
-                    <?php cybernauci_post_thumbnail(); ?>
+                        <?php cybernauci_post_thumbnail(); ?>
 
-                    <div class="entry-content">
-                        <?php
-                        /* translators: %s: Name of current post */
-                        the_content(sprintf(
-                            __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
-                            get_the_title()
-                        ));
-
-                        wp_link_pages(array(
-                            'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'cybernauci') . '</span>',
-                            'after' => '</div>',
-                            'link_before' => '<span>',
-                            'link_after' => '</span>',
-                            'pagelink' => '<span class="screen-reader-text">' . __('Page', 'cybernauci') . ' </span>%',
-                            'separator' => '<span class="screen-reader-text">, </span>',
-                        ));
-                        ?>
-                    </div><!-- .entry-content -->
-
-                    <footer class="entry-footer">
-                        <?php cybernauci_entry_meta(); ?>
-                        <?php
-                        edit_post_link(
-                            sprintf(
+                        <div class="entry-content">
+                            <?php
                             /* translators: %s: Name of current post */
-                                __('Edit<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
+                            the_content(sprintf(
+                                __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
                                 get_the_title()
-                            ),
-                            '<span class="edit-link">',
-                            '</span>'
-                        );
-                        ?>
-                    </footer><!-- .entry-footer -->
-                </article><!-- #post-## -->
-                <?
-            endwhile;
-            ?>
+                            ));
+
+                            wp_link_pages(array(
+                                'before' => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'cybernauci') . '</span>',
+                                'after' => '</div>',
+                                'link_before' => '<span>',
+                                'link_after' => '</span>',
+                                'pagelink' => '<span class="screen-reader-text">' . __('Page', 'cybernauci') . ' </span>%',
+                                'separator' => '<span class="screen-reader-text">, </span>',
+                            ));
+                            ?>
+                        </div><!-- .entry-content -->
+
+                        <footer class="entry-footer">
+                            <?php cybernauci_entry_meta(); ?>
+                            <?php
+                            edit_post_link(
+                                sprintf(
+                                /* translators: %s: Name of current post */
+                                    __('Edit<span class="screen-reader-text"> "%s"</span>', 'cybernauci'),
+                                    get_the_title()
+                                ),
+                                '<span class="edit-link">',
+                                '</span>'
+                            );
+                            ?>
+                        </footer><!-- .entry-footer -->
+                    </article><!-- #post-## -->
+                    <?
+                endwhile;
+                ?>
+            </div>
         </div>
     </div>
 </div>
--- wp-content/themes/cybernauci/single.php
+++ wp-content/themes/cybernauci/single.php
@@ -12,86 +12,89 @@
                 <h2 class="entry-title">Najnowsze informacje o programie</h2>
             </header>
         </div>
-    </div>
 
-    <div class="aktualnosci-list container-block">
-        <div class="container mainblock">
-            <div class="col-xs-12 col-md-2">
-                <a title="wróć do listy" rel="bookmark"
-                   href="<?php echo esc_url(get_permalink(get_page_by_title('Aktualności'))); ?>" class="back-to-list">wróć
-                    do listy</a>
-                <? if (is_singular('attachment')) {
-                    the_post_navigation(array(
-                        'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'cybernauci'),
-                    ));
-                } ?>
-            </div>
-            <div class="col-xs-12 col-md-6 col-md-offset-1">
-                <?php
-                while (have_posts()) : the_post();
-                    get_template_part('template-parts/content-aktualnosci', 'single');
+        <div class="aktualnosci-list">
+            <div class="container mainblock">
+                <div class="col-xs-12 col-md-2">
+                    <a title="wróć do listy" rel="bookmark"
+                       href="<?php echo esc_url(get_permalink(get_page_by_title('Aktualności'))); ?>"
+                       class="back-to-list">wróć
+                        do listy</a>
+                    <? if (is_singular('attachment')) {
+                        the_post_navigation(array(
+                            'prev_text' => _x('<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'cybernauci'),
+                        ));
+                    } ?>
+                </div>
+                <div class="col-xs-12 col-md-6 col-md-offset-1">
+                    <?php
+                    while (have_posts()) : the_post();
+                        get_template_part('template-parts/content-aktualnosci', 'single');
 
-                    $prev_post = get_previous_post();
-                    $next_post = get_next_post();
+                        $prev_post = get_previous_post();
+                        $next_post = get_next_post();
 
-                    if (!empty($prev_post) || !empty($next_post)) {
-                        echo '<h3 class="more-article">Czytaj również</h3><div class="row">';
-                        if (!empty($prev_post)) {
-                            print_r($prev_post) ?>
-                            <div class="col-xs-12 col-md-6">
-                                <article id="post-<?php echo $prev_post->ID ?>">
-                                    <header class="entry-header">
-                                        <?php $image = get_field('Image', $prev_post->ID);
-                                        if (!empty($image)) { ?>
-                                            <img class="img-responsive" src="<?php echo $image['url']; ?>" alt=""/>
-                                        <?php } ?>
-                                        <small><?php echo get_the_date('d.m.Y', $prev_post->ID); ?></small>
-                                        <h2 class="entry-title"><a
-                                                href="<? echo esc_url(get_permalink($prev_post->ID)); ?>"
-                                                rel="bookmark"><?php echo get_the_title($prev_post->ID); ?></a></h2>
-                                    </header>
+                        if (!empty($prev_post) || !empty($next_post)) {
+                            echo '<h3 class="more-article">Czytaj również</h3><div class="row">';
+                            if (!empty($prev_post)) {
+                                print_r($prev_post) ?>
+                                <div class="col-xs-12 col-md-6">
+                                    <article id="post-<?php echo $prev_post->ID ?>">
+                                        <header class="entry-header">
+                                            <?php $image = get_field('Image', $prev_post->ID);
+                                            if (!empty($image)) { ?>
+                                                <img class="img-responsive" src="<?php echo $image['url']; ?>" alt=""/>
+                                            <?php } ?>
+                                            <small><?php echo get_the_date('d.m.Y', $prev_post->ID); ?></small>
+                                            <h2 class="entry-title"><a
+                                                    href="<? echo esc_url(get_permalink($prev_post->ID)); ?>"
+                                                    rel="bookmark"><?php echo get_the_title($prev_post->ID); ?></a></h2>
+                                        </header>
 
-                                    <div class="entry-content">
-                                        <?php echo wp_trim_words($prev_post->post_content, 30) ?>
-                                    </div>
+                                        <div class="entry-content">
+                                            <?php echo wp_trim_words($prev_post->post_content, 30) ?>
+                                        </div>
 
-                                    <footer class="entry-footer">
-                                        <a class="readmore" href="<?php the_permalink($prev_post->ID) ?>" rel="bookmark"
-                                           title="<?php echo sprintf(__('Continue reading %s', 'cybernauci'), get_the_title($prev_post->ID)) ?>"><? echo __('czytaj więcej', 'cybernauci') ?></a>
-                                    </footer>
-                                </article>
-                            </div>
-                        <? }
-                        if (!empty($next_post)) { ?>
-                            <div class="col-xs-12 col-md-6">
-                                <article id="post-<?php echo $next_post->ID ?>">
-                                    <header class="entry-header">
-                                        <?php $image = get_field('Image', $next_post->ID);
-                                        if (!empty($image)) { ?>
-                                            <img class="img-responsive" src="<?php echo $image['url']; ?>" alt=""/>
-                                        <?php } ?>
-                                        <small><?php echo get_the_date('d.m.Y', $next_post->ID); ?></small>
-                                        <h2 class="entry-title"><a
-                                                href="<? echo esc_url(get_permalink($next_post->ID)); ?>"
-                                                rel="bookmark"><?php echo get_the_title($next_post->ID); ?></a></h2>
-                                    </header>
+                                        <footer class="entry-footer">
+                                            <a class="readmore" href="<?php the_permalink($prev_post->ID) ?>"
+                                               rel="bookmark"
+                                               title="<?php echo sprintf(__('Continue reading %s', 'cybernauci'), get_the_title($prev_post->ID)) ?>"><? echo __('czytaj więcej', 'cybernauci') ?></a>
+                                        </footer>
+                                    </article>
+                                </div>
+                            <? }
+                            if (!empty($next_post)) { ?>
+                                <div class="col-xs-12 col-md-6">
+                                    <article id="post-<?php echo $next_post->ID ?>">
+                                        <header class="entry-header">
+                                            <?php $image = get_field('Image', $next_post->ID);
+                                            if (!empty($image)) { ?>
+                                                <img class="img-responsive" src="<?php echo $image['url']; ?>" alt=""/>
+                                            <?php } ?>
+                                            <small><?php echo get_the_date('d.m.Y', $next_post->ID); ?></small>
+                                            <h2 class="entry-title"><a
+                                                    href="<? echo esc_url(get_permalink($next_post->ID)); ?>"
+                                                    rel="bookmark"><?php echo get_the_title($next_post->ID); ?></a></h2>
+                                        </header>
 
-                                    <div class="entry-content">
-                                        <?php echo wp_trim_words($next_post->post_content, 30) ?>
-                                    </div>
+                                        <div class="entry-content">
+                                            <?php echo wp_trim_words($next_post->post_content, 30) ?>
+                                        </div>
 
-                                    <footer class="entry-footer">
-                                        <a class="readmore" href="<?php the_permalink($next_post->ID) ?>" rel="bookmark"
-                                           title="<?php echo sprintf(__('Continue reading %s', 'cybernauci'), get_the_title($next_post->ID)) ?>"><? echo __('czytaj więcej', 'cybernauci') ?></a>
-                                    </footer>
-                                </article>
-                            </div>
-                            <?
+                                        <footer class="entry-footer">
+                                            <a class="readmore" href="<?php the_permalink($next_post->ID) ?>"
+                                               rel="bookmark"
+                                               title="<?php echo sprintf(__('Continue reading %s', 'cybernauci'), get_the_title($next_post->ID)) ?>"><? echo __('czytaj więcej', 'cybernauci') ?></a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <?
+                            }
+                            echo '</div>';
                         }
-                        echo '</div>';
-                    }
-                endwhile;
-                ?>
+                    endwhile;
+                    ?>
+                </div>
             </div>
         </div>
     </div>
