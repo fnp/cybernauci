45c7be52d5681e6f516e45ff099f9bd713be26da
Cain-Ish <mariusz.machuta@gmail.com>
2016-06-15 00:01:21 +0200

    dodawanie katalogu

---
+++ wp-content/themes/cybernauci/page-dodaj-katalog.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+    <div id="katalogDodaj">
+        <div class="info container-block">
+            <div class="container mainblock">
+                <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-left-computer.png"
+                     class="sidebanner sidebanner-left" alt=""/>
+                <img src="<?php bloginfo('template_directory'); ?>/img/sidebanner-right-rockets.png"
+                     class="sidebanner sidebanner-right" alt=""/>
+
+                <header class="entry-header">
+                    <h2 class="entry-title">Dodawanie katalogu</h2>
+                </header>
+                <div class="entry-content">
+                    <div class="col-xs-12 col-md-8 col-md-offset-2">
+                        <?php
+                        while (have_posts()) : the_post();
+                            the_content();
+                        endwhile;
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
